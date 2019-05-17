c1f28137705b40d8b0575aa2bedf9dfb4fc10f80
MagicianEssen <50726555+MagicianEssen@users.noreply.github.com>
2019-05-16 23:51:37 -0500

    Add files via upload

---
+++ skeleton/part 2/neural_network.py
@@ -0,0 +1,267 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+def plot_confusion_matrix(y_true, y_pred, classes,
+                          normalize=False,
+                          title=None,
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    from sklearn.metrics import confusion_matrix
+    from sklearn.utils.multiclass import unique_labels
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+    classes = classes[unique_labels(y_true, y_pred)]
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    return ax
+
+def init_weights(d, dp):
+    return 0.01 * np.random.uniform(0.0, 1.0, (d, dp)), np.zeros(dp)
+"""
+    Minigratch Gradient Descent Function to train model
+    1. Format the data
+    2. call four_nn function to obtain losses
+    3. Return all the weights/biases and a list of losses at each epoch
+    Args:
+        epoch (int) - number of iterations to run through neural net
+        w1, w2, w3, w4, b1, b2, b3, b4 (numpy arrays) - starting weights
+        x_train (np array) - (n,d) numpy array where d=number of features
+        y_train (np array) - (n,) all the labels corresponding to x_train
+        num_classes (int) - number of classes (range of y_train)
+        shuffle (bool) - shuffle data at each epoch if True. Turn this off for testing.
+    Returns:
+        w1, w2, w3, w4, b1, b2, b3, b4 (numpy arrays) - resulting weights
+        losses (list of ints) - each index should correspond to epoch number
+            Note that len(losses) == epoch
+    Hints:
+        Should work for any number of features and classes
+        Good idea to print the epoch number at each iteration for sanity checks!
+        (Stdout print will not affect autograder as long as runtime is within limits)
+"""
+def minibatch_gd(epoch, w1, w2, w3, w4, b1, b2, b3, b4, x_train, y_train, num_classes, shuffle=True):
+
+    #IMPLEMENT HERE
+    batchsize = 200
+    #already initialized w and b
+    losses = []
+    for e in range(epoch):
+        #randomization is consistent for both your x_train and y_train.
+        loss = 0
+        print(e)
+        #index = np.zeros(len(x_train))
+        #for i in range(len(x_train)):
+        #    index[i] = int(i)
+        x_new = x_train.copy()
+        y_new = y_train.copy()
+        if shuffle:
+            index = np.random.choice(len(x_train), len(x_train), replace=False)
+            #np.random.shuffle(index)
+            x_new = x_train[index]
+            y_new = y_train[index]
+
+        for i in range(int(x_train.shape[0]/batchsize)):
+            x = x_new[i*batchsize:(i+1)*batchsize]
+            y = y_new[i*batchsize:(i+1)*batchsize]
+            temploss = four_nn(x, w1, w2, w3, w4, b1, b2, b3, b4, y, False)
+            loss += temploss
+        print("loss",loss)
+        losses.append(loss)
+
+
+    return w1, w2, w3, w4, b1, b2, b3, b4, losses
+
+"""
+    Use the trained weights & biases to see how well the nn performs
+        on the test data
+    Args:
+        All the weights/biases from minibatch_gd()
+        x_test (np array) - (n', d) numpy array
+        y_test (np array) - (n',) all the labels corresponding to x_test
+        num_classes (int) - number of classes (range of y_test)
+    Returns:
+        avg_class_rate (float) - average classification rate
+        class_rate_per_class (list of floats) - Classification Rate per class
+            (index corresponding to class number)
+    Hints:
+        Good place to show your confusion matrix as well.
+        The confusion matrix won't be autograded but necessary in report.
+"""
+def test_nn(w1, w2, w3, w4, b1, b2, b3, b4, x_test, y_test, num_classes):
+
+
+    # for report: show confusion matrix
+    classifications = [[0,0,0,0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0,0,0,0],   
+                       [0,0,0,0,0,0,0,0,0,0]]
+    class_names = np.array(["T-shirt/top", "Trouser", "Pullover", "Dress",
+                            "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"])
+    pred = y_test.copy()
+    for i in range(1000):
+        pred[i] = random.randint(0, 9)
+    plot_confusion_matrix(y_test, pred, classes=class_names,
+                          normalize=True, title='Confusion matrix EXTRA CREDIT')
+    plt.show()
+    return avg_class_rate, class_rate_per_class
+
+"""
+    4 Layer Neural Network
+    Helper function for minibatch_gd
+    Up to you on how to implement this, won't be unit tested
+    Should call helper functions below
+"""
+def four_nn(x, w1, w2, w3, w4, b1, b2, b3, b4, y, test):
+    z1, acache1 = affine_forward(x, w1, b1)
+    a1, rcache1 = relu_forward(z1)
+    z2, acache2 = affine_forward(a1, w2, b2)
+    a2, rcache2 = relu_forward(z2)
+    z3, acache3 = affine_forward(a2, w3, b3)
+    a3, rcache3 = relu_forward(z3)
+    f,  acache4 = affine_forward(a3, w4, b4)
+    if test == True:
+        classifications = np.argmax(f,axis=1)#axis
+        return classifications
+    loss, dF = cross_entropy(f,y)
+    dA3, dW4, db4 = affine_backward(dF, acache4)
+    dZ3 = relu_backward(dA3, rcache3)
+    dA2, dW3, db3 = affine_backward(dZ3, acache3)
+    dZ2 = relu_backward(dA2, rcache2)
+    dA1, dW2, db2 = affine_backward(dZ2, acache2)
+    dZ1 = relu_backward(dA1, rcache1)
+    dX, dW1, db1 = affine_backward(dZ1, acache1)
+
+    eta = 0.1
+    #w1 = w1- eta*dW1
+    #w2 = w2- eta*dW2
+    #w3 = w3- eta*dW3
+    #w4 = w4- eta*dW4
+    #b1 = b1- eta*db1
+    #b2 = b2- eta*db2
+    #b3 = b3- eta*db3
+    #b4 = b4- eta*db4
+    #w1 = w1 - (eta * dW1)
+    w1 -= eta * dW1
+    w2 -= eta * dW2
+    w3 -= eta * dW3
+    w4 -= eta * dW4
+    b1 -= eta * db1
+    b2 -= eta * db2
+    b3 -= eta * db3
+    b4 -= eta * db4
+    return loss
+    #pass
+
+
+"""
+    Next five functions will be used in four_nn() as helper functions.
+    All these functions will be autograded, and a unit test script is provided as unit_test.py.
+    The cache object format is up to you, we will only autograde the computed matrices.
+
+    Args and Return values are specified in the MP docs
+    Hint: Utilize numpy as much as possible for max efficiency.
+        This is a great time to review on your linear algebra as well.
+"""
+def affine_forward(A, W, b):
+    Z = np.matmul(A,W)+b
+    cache = (A,W,b)
+    return Z, cache
+
+def affine_backward(dZ, cache):
+    A,W,b = cache
+    # Z(i,j), W(k,j), A(i,k)
+    dA = np.dot(dZ, W.T)
+    dW = np.dot(A.T, dZ)
+    dB = np.sum(dZ,0)
+    return dA, dW, dB
+
+def relu_forward(Z):
+    #Z(n,d)
+    A = Z.copy()
+    A[A<0]=0
+    #for n in range(A.shape[0]):
+    #    for d in range(A.shape[1]):
+    #        A[n][d] = max(A[n][d],0)
+    cache = Z
+    return A, cache
+
+def relu_backward(dA, cache):
+    Z = cache
+    dA[Z<0] = 0
+    return dA
+
+#def cross_entropy(F, y):
+#    n = F.shape[0]
+#    outersum = 0
+#    for i in range(n):
+#        logsum = np.log(np.sum(np.exp(F[i])))
+#        outersum += F[i,int(y[i])] - logsum
+#    loss = (-1/n) * outersum
+#
+#    dF = F.copy()
+#    for i in range(dF.shape[0]):
+#        for j in range(dF.shape[1]):
+#            factor = np.exp(F[i][j])/np.sum(np.exp(F[i]))
+#            dF[i][j] = (-1/n)*(1*(j==y[i])-factor)
+#
+#    return loss, dF
+def cross_entropy(F, y):
+    n = len(F)
+    exp_F = np.exp(F)
+    sum_exp_F = np.sum(exp_F, axis=1)
+    Fiyi = F[np.arange(n), y.astype(int)]
+    log_stuff = np.log(sum_exp_F)
+    loss = (-1 / n) * np.sum(Fiyi - log_stuff)
+
+    first_item = np.zeros(F.shape)
+    first_item[np.arange(n), y.astype(int)] = 1
+    second_item = exp_F / sum_exp_F.reshape((-1, 1))
+    dF = (-1 / n) * (first_item - second_item)
+
+    return loss, dF
+
